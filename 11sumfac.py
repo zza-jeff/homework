08892729c93e39e0e405305d52f3815575509331
Ziang Zhang <ziazhang@ucdavis.edu>
2022-01-20 13:02:53 -0800

    homework11: initial commit

---
+++ 11sumfac.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Define n
+
+n = 5
+
+# Store Outputs
+
+SUM = 0
+FACR = 1
+
+# Loop
+
+for i in range(1,n+1):
+    SUM = SUM + i
+    FACR = FACR * i
+
+print(n,SUM,FACR)
