85ded681392c66c0974aa3d1e7ab2a55e1b24885
Ziang Zhang <ziazhang@ucdavis.edu>
2022-01-26 21:30:42 -0800

    homework23: initial commit

---
+++ 23anti.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Write a program that prints the reverse-complement of a DNA sequence
+# You must use a loop and conditional
+
+dna = 'ACTGAAAAAAAAAAA'
+s = len(dna)
+anti = ""
+
+for i in range(s -1, -1, -1):
+    codon = dna[i]
+    if codon == "A":
+        anti += 'T'
+    elif codon == 'T':
+        anti += 'A'
+    elif codon == 'G':
+        anti += 'C'
+    else:
+        anti += 'G'
+
+print(anti)
