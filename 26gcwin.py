4301061e22c1998f7551e8d4122d3ee2245a33d4
Ziang Zhang <ziazhang@ucdavis.edu>
2022-01-30 12:51:28 -0800

    homework26: initial commit

---
+++ 26gcwin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the GC fraction of a DNA sequence in a window
+# Window size is 11 nt
+# Output with 4 significant figures using whichever method you prefer
+# Use nested loops
+
+seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
+w = 11
+
+
+for i in range(len(seq)-10):
+    pt = 0
+    out = 0 # Put them inside the loop so they will be refreshed.
+    wind = seq[i:i + w]
+    for x in range(w):
+        if wind[x] == 'G' or wind[x] == 'C':
+            pt += 1
+            out = pt / 11
+    print(i, wind, f'{out:.4f}')
