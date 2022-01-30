7787756fc65148c7ce3c2ac27267c18647dc5b6f
Ziang Zhang <ziazhang@ucdavis.edu>
2022-01-30 14:04:06 -0800

    homework27: initial commit

---
+++ 27gcwin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Write a program that computes the GC fraction of a DNA sequence in a window
+# Window size is 11 nt
+# Output with 4 significant figures using whichever method you prefer
+# Use no nested loops. Instead, count only the first window
+# Then 'move' the window by adding 1 letter on one side
+# And subtracting 1 letter from the other side
+# Describe the pros/cons of this algorith vs. nested loops
+
+seq = 'ACGACGCAGGAGGAGAGTTTCAGAGATCACGAATACATCCATATTACCCAGAGAGAG'
+w = 11
+pt = 0
+out = 0
+
+wind = seq[0:11] # Initial Slice
+
+for x in range(w):
+    if wind[x] == 'G' or wind[x] == 'C':
+        pt += 1
+
+for i in range(0, len(seq) - 10):
+    if i == 0: out = pt / 11 # If i = 0, do nothing.
+    else:
+        if wind[0] == 'G' or wind[0] == 'C':
+            pt -= 1
+        wind = wind[1:11]
+        wind += seq[i+10]
+        if wind[10] == 'G' or wind[10] == 'C':
+            pt += 1
+        out = pt / 11
+    print(i, wind, '{:.4f}'.format(out))
