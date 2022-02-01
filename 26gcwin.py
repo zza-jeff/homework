9ad99c6c5e832c409f95bd32b6a84a0155398ca0
Ziang Zhang <ziazhang@ucdavis.edu>
2022-02-01 10:38:08 -0800

    homework26: modify comment

--- 26gcwin.py
+++ 26gcwin.py
@@ -11,10 +11,12 @@ w = 11
 
 for i in range(len(seq) - w + 1):
     gc = 0
-    out = 0 # Put them inside the loop so they will be refreshed every cycle.
+    out = 0 # Put them inside the loop as they only be used in it.
     wind = seq[i:i + w]
     for x in range(w):
         if wind[x] == 'G' or wind[x] == 'C':
             gc += 1
             out = gc / w
     print(i, wind, f'{out:.4f}')
+
+# Discussed with Jan
