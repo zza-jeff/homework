ac600c8d480192bd7ef0303d13c7aac6e7fd04e4
Ziang Zhang <ziazhang@ucdavis.edu>
2022-02-08 11:25:16 -0800

    homework31: add tolorance verification

--- 31entropy.py
+++ 31entropy.py
@@ -9,11 +9,17 @@
 import math
 import sys
 
+tolorance = 0.001
+
 # First, change input into float, then transfer into a new list
 prob = [float(sys.argv[i]) for i in range(1, len(sys.argv))]
 
+if abs(sum(prob) - 1) > tolorance:
+    print('Probability sum is not equal to 1, exit.')
+    sys.exit()
+
 # Calculate entropy in a new list
-entro = [prob[x] * math.log(prob[x]) for x in range(len(prob))]
+entro = [prob[x] * math.log2(prob[x]) for x in range(len(prob))]
 
 shannon = -sum(entro)
 
