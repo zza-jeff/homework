7cfdaf6a3a4ca2e292cacd77d6f90ef9931e513e
Ziang Zhang <ziazhang@ucdavis.edu>
2022-02-22 21:30:12 -0800

    homework50: initial commit

---
+++ 50kmers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# 50kmers.py
+
+import sys
+assert(len(sys.argv) == 3)
+
+# Make a program that reports the kmer counts for a fasta file
+# Your program should take 2 arguments:
+#    1. The file name
+#    2. The size of k
+
+seq = ''
+filename = sys.argv[1]
+k = int(sys.argv[2])
+count = {}
+total = 0
+
+with open(filename, 'r') as fp:
+    for line in fp.readlines():
+        if line[0] != '>':
+            seq += line.rstrip('\n')
+
+for i in range(len(seq) - k + 1):
+    slice = seq[i:i+k]
+    if slice not in count:
+        count[slice] = 0
+        count[slice] += 1
+        total += 1
+    else:
+        count[slice] += 1
+        total += 1
+
+for x, y in sorted(count.items(), key=lambda item: item[0]):
+    print(f'{x}\t{y}\t{y/total:.4f}')
