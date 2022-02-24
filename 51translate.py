8bb71e85beead4d98afdc64f4dd4c2d0e4b58394
Ziang Zhang <ziazhang@ucdavis.edu>
2022-02-23 16:46:29 -0800

    homework51: initial commit

---
+++ 51translate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# 51translate.py
+
+import sys
+assert(len(sys.argv) == 2)
+
+# Make a program that translates coding sequences into proteins
+# You have been provided with the genetic code as a dictionary
+# Use the actin sequence in the Data directory
+
+gcode = {
+	'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+	'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+	'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+	'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+	'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+	'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+	'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+	'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+	'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+	'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+	'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+	'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+	'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+	'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+	'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+	'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+}
+
+filename = sys.argv[1]
+seq = ''
+
+with open(filename, 'r') as fp:
+    for line in fp.readlines():
+        if line[0] != '>':
+            seq += line.rstrip('\n')
+
+seq = seq.upper()
+
+def translate(seq):
+	aachain = ''
+	for i in range(0, len(seq) - 2, 3):
+		frame = seq[i:i+3]
+		if frame in gcode: aachain += gcode[frame]
+	return aachain
+
+print(translate(seq))
