7f07ad25587108824e4a2ff8e1ba35265ef52024
Ziang Zhang <ziazhang@ucdavis.edu>
2022-03-07 21:00:09 -0800

    homework61: fix uneven tab

--- 61dust.py
+++ 61dust.py
@@ -18,13 +18,12 @@ import mcb185
 
 parser = argparse.ArgumentParser(description='Output FASTA format with low entropy masking')
 parser.add_argument('--f', required=True, type=str,
-	metavar='<str>', help='require a string for file')
+    metavar='<str>', help='require a string for file')
 parser.add_argument('--w', required=True, type=int,
-	metavar='<int>', help='require integer for window size')
+    metavar='<int>', help='require integer for window size')
 parser.add_argument('--e', required=True, type=float,
-	metavar='<float>', help='require floating point for entropy threshold')
-parser.add_argument('--nmask', action='store_true',
-	help='N-based masking')
+    metavar='<float>', help='require floating point for entropy threshold')
+parser.add_argument('--nmask', action='store_true', help='N-based masking')
 arg = parser.parse_args()
 
 mask = ''
@@ -32,15 +31,15 @@ for n in range(arg.w):
     mask += 'N'
 
 for name, seq in mcb185.read_fasta(arg.f):
-	entropy = {}
-		
-	for i in range(0, len(seq) - arg.w + 1):
-	    window = seq[i:i+arg.w]
-	    prob = mcb185.compos(window)
-	    if i not in entropy: entropy[i] = mcb185.shannon(prob) # store entropy with starting position
-	
-	for c, cal in entropy.items():
-		if entropy[c] < arg.e: # if below threshold starting at position "c", replace sequence
-			if arg.nmask: seq = seq.replace(seq[c:c+arg.w], mask)
-			else: seq = seq.replace(seq[c:c+arg.w], seq[c:c+arg.w].lower())
-	print(f'{name}\n{seq}')
+    entropy = {}
+    
+    for i in range(0, len(seq) - arg.w + 1):
+        window = seq[i:i+arg.w]
+        prob = mcb185.compos(window)
+        if i not in entropy: entropy[i] = mcb185.shannon(prob) # store entropy with starting position
+    
+    for c, cal in entropy.items():
+        if entropy[c] < arg.e: # if below threshold starting at position "c", replace sequence
+            if arg.nmask: seq = seq.replace(seq[c:c+arg.w], mask)
+            else: seq = seq.replace(seq[c:c+arg.w], seq[c:c+arg.w].lower())
+    print(f'{name}\n{seq}')
