e57270d32619058b7b7ad712010cdac4f926ddf5
Ziang Zhang <ziazhang@ucdavis.edu>
2022-03-08 20:12:21 -0800

    findmissingkmer: use itertools

--- missingkmer.py
+++ missingkmer.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
-import random
-import math
+import itertools
 import mcb185
 
-parser = argparse.ArgumentParser(description='Brief description of program.')
+
+parser = argparse.ArgumentParser(description='find kmer(s) not present')
 parser.add_argument('--seq', required=True, type=str,
 	metavar='<str>', help='require a string to locate the file')
-parser.add_argument('--len', required=True, type=int,
-	metavar='<int>', help='require integer for null sequence length')
-parser.add_argument('--kmer', required=True, type=int,
-	metavar='<int>', help='require integer for kmer length')
 arg = parser.parse_args()
 
-kmer = {}
-nullseq = ''
-
-base = "ATGC"
-
-for i in range(arg.len):
-    nullseq += random.choice(base)
-
-for a in range(len(nullseq) - arg.kmer + 1):
-    window = nullseq[a:a+arg.kmer]
-    if window not in kmer: kmer[window] = 0
+result = []
 
 for name, seq in mcb185.read_fasta(arg.seq):
-    for b in range(len(seq) - arg.kmer + 1):
-        frame = seq[b:b+arg.kmer]
-        if frame in kmer: kmer[frame] += 1
+    for i in range(len(seq)):
+        kmer = {''.join(nt): 0 for nt in itertools.product('ATGC', repeat=i+1)}
+        
+        for b in range(len(seq) - i):
+            frame = seq[b:b+i+1]
+            if frame in kmer: kmer[frame] += 1
+        
+        for mer, k in kmer.items():
+            if k == 0: result.append(mer)
+        
+        if len(result) != 0:
+            result.append(f'{"k="}{i+1}')
+            break
 
-for poss, n in kmer.items():
-    if n == 0: print(poss)
+for a in range(len(result)):
+    print(result[a])
