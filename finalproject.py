bd80d28d33c609019419199d573e5d6106cf408c
Ziang Zhang <ziazhang@ucdavis.edu>
2022-03-10 12:35:20 -0800

    finalproject: try to ditch random sequence generation

--- finalproject.py
+++ finalproject.py
@@ -9,22 +9,25 @@ import mcb185
 parser = argparse.ArgumentParser(description='Brief description of program.')
 parser.add_argument('--seq', required=True, type=str,
 	metavar='<str>', help='require a string to locate the file')
-parser.add_argument('--len', required=True, type=int,
-	metavar='<int>', help='require integer for null protein length')
 arg = parser.parse_args()
 
 aa = 'ACDEFGHIKLMNPQRSTVWY'
-nullseq = ''
 result = {}
 
+"""
+# null sequence (deprecated)
+nullseq = ''
 for i in range(arg.len):
-    nullseq += random.choice(aa) # picking up a letter from 20 aa each time
+    nullseq += random.choice(aa)
 nullstat = mcb185.compos(nullseq)
-# so the random sequence is made in a IID process, use as null model
+"""
 
 for name, seq in mcb185.read_fasta(arg.seq):
 	seq = seq.rstrip('*')
 	stat = mcb185.compos(seq)
-	kldiverge = {x: stat[x] * math.log(stat[x] / nullstat[x]) for x, n in stat.items()}
+	kldiverge = {x: n * math.log(n / (1 / len(aa))) for x, n in stat.items()}
+	# if IID, every letter should have probability of 1/20
 	if name not in result: result[name] = sum(kldiverge.values())
-	print(f'\n{name}\n{result[name]}\n{kldiverge}\n')
+	print(f'\n{name}\n{"k-l divergence = "}{result[name]:.6f}\n')
+	for l, div in sorted(kldiverge.items()):
+	    print(f'{l}\t{div:.6f}')
