07a2f3b329ea647625dae681a400d767318f0468
Ziang Zhang <ziazhang@ucdavis.edu>
2022-03-10 20:52:08 -0800

    finalproject: add code to compare between sequences

--- finalproject.py
+++ finalproject.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 # final project: independent and identically distributed
+# The program is able to: 1. Compare single protein to null model; 2. Compare single protein with other proteins in file
 
 import argparse
-import random
+import itertools
 import math
 import mcb185
 
-parser = argparse.ArgumentParser(description='Brief description of program.')
+parser = argparse.ArgumentParser(description='Validate IID')
 parser.add_argument('--seq', required=True, type=str,
 	metavar='<str>', help='require a string to locate the file')
 arg = parser.parse_args()
 
-aa = 'ACDEFGHIKLMNPQRSTVWY'
-result = {}
+letter = 'ACDEFGHIKLMNPQRSTVWY'
 
 """
 # null sequence (deprecated)
@@ -22,12 +22,13 @@ for i in range(arg.len):
 nullstat = mcb185.compos(nullseq)
 """
 
-for name, seq in mcb185.read_fasta(arg.seq):
-	seq = seq.rstrip('*')
-	stat = mcb185.compos(seq)
-	kldiverge = {x: n * math.log(n / (1 / len(aa))) for x, n in stat.items()}
-	# if IID, every letter should have probability of 1/20
-	if name not in result: result[name] = sum(kldiverge.values())
-	print(f'\n{name}\n{"k-l divergence = "}{result[name]:.6f}\n')
-	for l, div in sorted(kldiverge.items()):
-	    print(f'{l}\t{div:.6f}')
+protein = {name: seq.rstrip('*') for name, seq in mcb185.read_fasta(arg.seq)}
+
+for id, aa in protein.items():
+    stat = mcb185.compos(aa)
+    nullkldiverage = {x: n * math.log(n / (1 / len(letter))) for x, n in stat.items()}
+    print(f'\n\n{id}\n{"k-l divergence to null model = "}{sum(nullkldiverage.values()):.8f}\n')
+    for id1, aa1 in protein.items():
+        stat1 = mcb185.compos(aa1)
+        kldiverage = {x1: n1 * math.log(n1 / stat[x1]) for x1, n1 in stat1.items() if x1 in stat.keys()}
+        print(f'{id1[0:11]}\t{"k-l divergence to "}{id[0:11]}{" = "}{sum(kldiverage.values()):.8f}')
